083f5237ea45082bc556947d7be8bec92ef5fe4b
brady salz <brady.salz@gmail.com>
2016-01-07 17:34:52 -0500

    PEP8 good, tabs bad

--- models.py
+++ models.py
@@ -1,50 +1,57 @@
-from flask import Flask, db
+import datetime
+
 from teampls import db
 
-app = Flask(__name___)
 
 class MatchResult(db.Model):
-	# team match results for a player
-	
-	# match ID data
-	id = db.Column(db.Integer, primary_key=True)
-	date = db.Column(db.DateTime)
-	match = db.Column(db.Integer)
-	summoner = db.Column(db.String(18)) # summoner name, all lowercase ('dahtguy')
-	mode = db.Column(db.String(25)) # game mode (NORMAL, SOLO_5X5, etc)
+    # team match results for a player
+    # comments refer to API tag
 
-	# game data
-	win_game = db.Column(db.Boolean) # True = Victory, False = Arnold
-	kills = db.Column(db.Integer)
-	deaths = db.Column(db.Integer)
-	assists = db.Column(db.Integer)
+    # match ID data
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime)        # createDate
+    match = db.Column(db.Integer)        # gameId
+    summoner = db.Column(db.String(18))  # summoner name, all lowercase ('dahtguy')
+    mode = db.Column(db.String(25))      # gameMode (NORMAL, SOLO_5X5, etc)
 
-	damage = db.Column(db.Integer) # totalDamageDealtToChampions
-	gold = db.Column(db.Integer)   # gold earned
-	wards = db.Column(db.Integer)  # wards placed
+    # game data
+    win_game = db.Column(db.Boolean)  # win (True = Victory, False = Arnold)
+    kills = db.Column(db.Integer)     # championsKilled
+    deaths = db.Column(db.Integer)    # numDeaths
+    assists = db.Column(db.Integer)   # assists
 
-	lane = db.Column(db.Integer) # playerPosition
-	role = db.Column(db.Integer) # playerRole
+    damage = db.Column(db.Integer)  # totalDamageDealtToChampions
+    gold = db.Column(db.Integer)    # gold [gold earned]
+    wards = db.Column(db.Integer)   # wardsPlaced
 
-	def __init__(self, date, match=0, summoner='', mode='', 
-					win_game=False, kills=0, deaths=0, assists=0,
-					damage=0, gold=0, wards=0, lane=0, role=0):
+    lane = db.Column(db.Integer)  # playerPosition
+    role = db.Column(db.Integer)  # playerRole
 
-		self.date  = date
-		self.match = match
-		self.summoner = summoner
-		self.mode = mode
-		self.win_game = win_game
-		self.kills = kills
-		self.deaths = deaths
-		self.assists = assists
-		self.damage = damage
-		self.gold = gold
-		self.wards = wards
-		self.lane = lane
-		self.role = role
+    def __init__(self, date=None, match=0, summoner='', mode='',
+                 win_game=False, kills=0, deaths=0, assists=0,
+                 damage=0, gold=0, wards=0, lane=0, role=0):
 
-	def __repr__(self):
-		won = 'won' if win_game else 'lost'
-		return '<Match {0} was {1} by Player {2}>'.format(match, won, summoner)
+        if date is None:
+            self.date = datetime.datetime.utcnow()
+        else:
+            self.date = date
+
+        self.match = match
+        self.summoner = summoner
+        self.mode = mode
+        self.win_game = win_game
+        self.kills = kills
+        self.deaths = deaths
+        self.assists = assists
+        self.damage = damage
+        self.gold = gold
+        self.wards = wards
+        self.lane = lane
+        self.role = role
+
+    def __repr__(self):
+        won = 'won' if self.win_game else 'lost'
+        summoner = 'N/A' if self.summoner == '' else self.summoner
+        match = 'N/A' if self.match == 0 else self.match
+        return '<Match {0} was {1} by Player {2}>'.format(match, won, summoner)
 
--- teampls.py
+++ teampls.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///matches.db'
 db = SQLAlchemy(app)
+
 
 @app.route('/')
 def hello_world():
