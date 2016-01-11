792a0d3e4f229e522c77fff37b04e22bf0db05fa
brady salz <brady.salz@gmail.com>
2016-01-10 20:19:17 -0500

    fetcher for cron job

---
+++ fetcher.py
@@ -0,0 +1,25 @@
+import sys
+import csv
+
+from riothelpers import check_recent_matches
+
+
+def fetch():
+    """Helper function to fetch matches. Used as cron job.
+    Takes the first argument on as a CSV list of summoners, in the format:
+    [ Real Name, Summoner Name, Summoner ID ]
+    ex: [ Brady, dahtguy, 111111 ]
+    """
+    file_name = sys.argv[1]
+
+    lookup = []
+    with open(file_name, 'rb') as f:
+        file_read = csv.reader(f)
+        for row in file_read:
+            lookup.append(row[2])
+
+    for id in lookup:
+        check_recent_matches(id)
+
+if __name__ == '__main__':
+    fetch()
